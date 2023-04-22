63967f596220178cdc0c798990ea28a85c37186c
IdoTauman <121692586+IdoTauman@users.noreply.github.com>
2023-04-22 14:42:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-insults = ['bad', 'stupid', 'dumb', 'who asked', 'a 3 year old', 'an anime kid', 'fat']
-
-ariel_roasts = ['imagine paying for a free game', 'imagine spoiling an entire anime, also, imagine watching anime', 'imagine fighting over ronbleks', 'imagine playing ronbleks when ur over 3']
-
 recipients = ['748981885258170389', '769139609187123240', '633687383660953603']
 
 last_dm = {}
@@ -35,9 +31,9 @@ async def on_ready():
 
     await tree.sync(guild=discord.Object(id=1049253865112997888))
 
-    aviv_venting_about_his_shitass_brothers = client.get_channel(1049386904065409054)
+    aviv_venting_about_his_brothers = client.get_channel(1049386904065409054)
     global last_message
-    async for message in aviv_venting_about_his_shitass_brothers.history(limit=1000):
+    async for message in aviv_venting_about_his_brothers.history(limit=1000):
         if message.author.id == 476686545034674176:
             last_message = message
 
@@ -55,13 +51,6 @@ async def on_message(msg):
     if msg.author == client.user:
         return
 
-    if msg.author.id == 633687383660953603:
-        message = f'pimed is {choice(insults)}'
-        if message == 'pimed is who asked':
-            await msg.channel.send('who asked')
-        else:
-            await msg.channel.send(message)
-
     if msg.author.id == 476686545034674176:
         if msg.channel.id == 1049386904065409054:
             global last_message
@@ -69,14 +58,8 @@ async def on_message(msg):
             for i in recipients:
                 user = await client.fetch_user(i)
                 if i not in last_dm or time.time() - last_dm[i] >= 600:
-                    await user.send('aviv is venting about his shitass brothers')
+                    await user.send('aviv is venting about his brothers')
                     last_dm[i] = time.time()
-
-@tree.command(name= 'roast_ariel', description= 'roast ariel', guild=discord.Object(id=1049253865112997888))
-async def roast_ariel(interaction):
-    log_channel = client.get_channel(1072481763454091314)
-    await interaction.response.send_message(f'{choice(ariel_roasts)} <@769139609187123240>')
-    await log_channel.send(interaction.user.name)
 
 @tree.command(name= 'last_vent', description= 'when did aviv last vent about his brothers', guild=discord.Object(id=1049253865112997888))
 async def last_vent(interaction):
@@ -84,18 +67,5 @@ async def last_vent(interaction):
     msg_time = last_message.created_at
 
     await interaction.response.send_message(f'aviv last vented at {dateToString(msg_time)} <@{interaction.user.id}>')
-
-@tree.command(name= 'ariel_roasting_leaderboard', description= 'show the people that roasted ariel the most', guild=discord.Object(id=1049253865112997888))
-async def leaderboard(interaction):
-    log_channel = client.get_channel(1072481763454091314)
-    log_history = log_channel.history(limit=1000)
-    log_list = []
-    async for m in log_history:
-        log_list.append(m.content)
-    log_authors = set(log_list)
-    leaderboard_list = []
-    for author in log_authors:
-        leaderboard_list.append(f'{author}: {log_list.count(author)}')
-    await interaction.response.send_message('\n'.join(leaderboard_list))
 
 client.run({token})
